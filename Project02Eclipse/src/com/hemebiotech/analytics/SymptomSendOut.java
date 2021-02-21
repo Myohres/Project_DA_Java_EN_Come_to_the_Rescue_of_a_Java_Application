97529392bae2e04cc7ffff4b41bba0800e328b34
Myohres <77498044+Myohres@users.noreply.github.com>
2021-02-21 15:49:35 +0100

    new java class SymptomSendOut

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomSendOut.java
@@ -0,0 +1,56 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+public class SymptomSendOut {
+    import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+    /** SymptomSendOut
+     * Ecrire les clés et valeurs du symptomTab dans un fichier de sortie OUT_PUT;
+     *
+     *
+     *
+     */
+
+    public class SymptomSendOut {
+
+        public static void sendOut (String OUTPUT_FILE, Map<String, Integer> symptomTab)  {
+
+            FileWriter symptomOut = null;
+            try {
+                symptomOut = new FileWriter(OUTPUT_FILE);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Fichier Symptom.out erreur");
+            }
+
+            Set listKeys = symptomTab.keySet();
+
+            for (Object key : listKeys) {
+                try {
+                    assert symptomOut != null;
+                    symptomOut.write(key + " : " + symptomTab.get(key) + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("Erreur ecriture tableau sur fichier.out");
+                }
+            }
+
+            try {
+                assert symptomOut != null;
+                symptomOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+}
