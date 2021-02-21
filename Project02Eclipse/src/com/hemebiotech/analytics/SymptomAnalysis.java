0fecdd90c9c13e3f7a40d6ba3dc87f316e1e23c0
Myohres <77498044+Myohres@users.noreply.github.com>
2021-02-21 15:42:39 +0100

    restart project with symptomanalysis class java

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomAnalysis.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class SymptomAnalysis {
+
+    public static final String INPUT_FILE = "symptoms.txt";
+    public static final String OUTPUT_FILE = "Symptom.out";
+
+
+
+
+    public static void main(String[] Args) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE));
+
+            Map<String, Integer> symptomTab = SymptomOrder.symptomTab(reader); // tableau contenant les symptoms comptés et triés
+
+            SymptomSendOut.sendOut(OUTPUT_FILE,symptomTab); // exportation du tableau sur fichier.out
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
