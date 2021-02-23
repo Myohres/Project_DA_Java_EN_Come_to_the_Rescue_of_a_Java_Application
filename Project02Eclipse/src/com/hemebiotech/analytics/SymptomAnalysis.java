5709b263a738a430f9a7b7787e51220aaa8a9173
Myohres <77498044+Myohres@users.noreply.github.com>
2021-02-23 23:35:08 +0100

    improve location and path symptom.txt

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomAnalysis.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomAnalysis.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class SymptomAnalysis {
 
-    public static final String INPUT_FILE = "Project02Eclipse/symptoms.txt";
+    public static final String INPUT_FILE = "symptoms.txt";
     public static final String OUTPUT_FILE = "Symptom.out";
 
 
@@ -15,12 +15,16 @@ public class SymptomAnalysis {
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE));
+            System.out.println("file " +INPUT_FILE + " read");
 
             Map<String, Integer> symptomTab = SymptomOrder.symptomTab(reader); // board with symptoms counted and indexed
+            System.out.println("Board symptomTab created");
 
             SymptomSendOut.sendOut(OUTPUT_FILE,symptomTab); // exportation of board to Symptom.out
+            System.out.println("Board symptomTab sent");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+
         }
 
     }
